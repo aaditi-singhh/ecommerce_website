e6e5b5d76abf5c8faef33c6c5551d12ba2a6ec60
aaditi-singhh <aditisingh6841@gmail.com>
2026-03-16 23:50:47 +0530

    1stday

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import { getProducts } from "../data/product";
+
+export default function Home(){
+    const products = getProducts();
+
+    return(
+    <div className="page">
+     <div className="home-hero">
+     <h1 className="home-title">  Welcome to ShopHub</h1>
+     <p className="home-subtitle"> 
+            Discover amazing products at great prices </p>
+
+
+    </div>
+
+    <div className="container"> 
+        <h2 className="page-title"> Our Products</h2>
+        <div className="products-grid">
+            {products.map}
+        </div>
+    </div>
+
+
+
+
+</div>
+    );
+}
