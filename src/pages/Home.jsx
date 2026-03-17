d1be4a4d57fdb508d3e82a24fc97dae3e03f075d
aaditi-singhh <aditisingh6841@gmail.com>
2026-03-17 22:41:07 +0530

    added home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,17 +17,7 @@ export default function Home(){
         <h2 className="page-title"> Our Products</h2>
         <div className="products-grid">
             {products.map((product) => (
-                <div className="product-card" key={product.id}>
-                   <img src={product.image} className="product-card-image" />
-                     <div className="product-card-content">
-                       <h3 className ="product-card-name"> {product.name}</h3>
-                       <p className ="product-card-price"> ${product.price}</p>
-                       <div className ="product-card-actions"> 
-                         <Link className='btn btn-secondary'> View Details</Link>
-                         <button className='btn btn-primary'> Add to cart </button>
-                        </div>
-                        </div>
-                    </div>
+               <ProductCard product ={product} key ={product.id}/>
             ))}
         </div>
     </div>
